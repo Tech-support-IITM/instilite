4a0360d2e5fa4b1c7a078fa07f07ce9fda13977f
praju-jo <160016282+praju-jo@users.noreply.github.com>
2024-02-22 15:56:43 +0530

    Update loginui.js

--- loginui.js
+++ loginui.js
@@ -9,7 +9,7 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  Image
+  Image // Import Image component
 } from 'react-native';
 
 const App = () => {
@@ -29,7 +29,6 @@ const App = () => {
       <Image style={styles.logo} source={{ uri: 'https://s3-alpha-sig.figma.com/img/bb35/1a0d/70ccf56eeabc718375f7898daf3890b9?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f8SOj983C6uPlCuqxcWv2xAk7jLcrrJwogU~mxee9XxfYfOlaFoMs1ZdWtD52eik3ObmDpajZNi5XEJxu4DP0P2oIv6iFA4HQzRv29caaYkWVCDB6kjH1DXVATSwb-AkxTEFwQd2FpR6lrp-ttapj9lwriN86lrrqP-PI0m6xA4gSAiVUwuULYqIwt9-i6ayYSvZuQyywfGY7thm9n3TSKLUcmruK7-07pFOz1XVnyd3oznjsxCr0l826NkHFOQEskSiNk1gD-irbuArvGvzJF1tDCx~BJRp8X1gU7Gjq8oHHV6Q9TlwyWFT-PzeBpaMWQF2J1YokIRkmPsDVEwiRg__' }} />
       <Text style={styles.name}>INSTILITE</Text>
       <Text style={styles.tagline}>Insti happening all at once</Text>
-      <Text style={styles.title}>Login</Text>
       <View style={styles.inputView}>
         <TextInput
           style={styles.inputText}
@@ -69,25 +68,20 @@ const styles = StyleSheet.create({
   },
   name: {
     fontWeight: 'bold',
-    fontSize: 80,
+    fontSize: 60,
     color: '#000000',
-    marginBottom: 60,
+    marginBottom: 40,
   },
   tagline: {
     fontSize: 30,
     color: '#000000',
     marginBottom: 50,
+    fontFamily: 'Georgia-BoldItalic'
   },
   logo: {
     width: 100,
     height: 100,
-    marginBottom: 60,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 50,
-    color: '#606D56',
-    marginBottom: 40,
+    marginBottom: 10,
   },
   inputView: {
     width: '80%',
